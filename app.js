9eebf683bfd7f2ccde7168a2bcd9ad1b374d0c26
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-08-25 16:45:10 -0700

    added noise click counters

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 // import functions
 const catImg = document.getElementById('cat');
 const catSound = document.getElementById('meow');
+const catSpan = document.getElementById('cat-span');
 
 const dogImg = document.getElementById('dog');
 const dogSound = document.getElementById('bark');
+const dogSpan = document.getElementById('dog-span');
 
 const horseImg = document.getElementById('horse');
 const horseSound = document.getElementById('neigh');
+const horseSpan = document.getElementById('horse-span');
 
+
+let catClicks = 0;
+let dogClicks = 0;
+let horseClicks = 0;
 
 // reference needed DOM elements
 
@@ -16,14 +23,21 @@ const horseSound = document.getElementById('neigh');
 
 catImg.addEventListener('click', () => {
     catSound.play();
+    catClicks ++;
+    catSpan.textContent = catClicks + ' meows';
 });
 
 dogImg.addEventListener('click', () => {
     dogSound.play();
+    dogClicks ++;
+    dogSpan.textContent = dogClicks + ' barks';
 });
 
 horseImg.addEventListener('click', () => {
     horseSound.play();
+    horseClicks ++;
+    horseSpan.textContent = horseClicks + ' neighs';
+    
 });
 
   // get user input(s)
